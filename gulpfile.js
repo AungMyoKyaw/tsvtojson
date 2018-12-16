da2ff8f7880c0f91f40018e150cfcb13d290c644
Aung Myo Kyaw <git@aungmyokyaw.com>
2018-12-16 21:42:20 +0630

    chore: prettier

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,27 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var mocha = require('gulp-mocha');
 
-gulp.task('transpile',function(){
-	return gulp.src('src/*.js')
-		.pipe(babel({
-			presets:['env']
-		}))
-		.pipe(gulp.dest('dist'))
-})
+gulp.task('transpile', function() {
+  return gulp
+    .src('src/*.js')
+    .pipe(
+      babel({
+        presets: ['env']
+      })
+    )
+    .pipe(gulp.dest('dist'));
+});
 
-gulp.task('test',function(){
-	return gulp.src('test/tsv2json.test.js')
-		.pipe(mocha())
-		.once('error',function(){
-			process.exit(1);
-		})
-		.once('end',function(){
-			process.exit();
-		})
-})
+gulp.task('test', function() {
+  return gulp
+    .src('test/tsv2json.test.js')
+    .pipe(mocha())
+    .once('error', function() {
+      process.exit(1);
+    })
+    .once('end', function() {
+      process.exit();
+    });
+});
 
-gulp.task('default',['transpile','test'])
+gulp.task('default', ['transpile', 'test']);
--- src/tsvtojson.js
+++ src/tsvtojson.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 
-const tsvtojson = (filepath,headers,splitString)=>{
-	return new Promise((resolve,reject)=>{
-		try{
-			let tsv = fs.readFileSync(filepath,'utf8');
-			let header = headers || [];
-			let json = [];
-			let splitString = splitString || '\n'
-			tsv.split(splitString).forEach((line,index)=>{
-				if(!index && !header.length){
-					header = line.split('\t');
-				} else {
-					let obj = {}
-					line.split('\t').forEach((value,index)=>{
-						value ? obj[header[index]] = value : '';
-					});
-					Object.keys(obj).length ? json.push(obj) : '';
-				}
-			});
-			resolve(json);
-		}
-		catch(err){
-			reject(err);
-		}
-	})
-}
+const tsvtojson = (filepath, headers, splitString) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let tsv = fs.readFileSync(filepath, 'utf8');
+      let header = headers || [];
+      let json = [];
+      let splitString = splitString || '\n';
+      tsv.split(splitString).forEach((line, index) => {
+        if (!index && !header.length) {
+          header = line.split('\t');
+        } else {
+          let obj = {};
+          line.split('\t').forEach((value, index) => {
+            value ? (obj[header[index]] = value) : '';
+          });
+          Object.keys(obj).length ? json.push(obj) : '';
+        }
+      });
+      resolve(json);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 module.exports = tsvtojson;
